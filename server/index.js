bdfba7f9a89c7552e8e9da65c3ab6bb12ac4ce76
Håvard Ranum <havard.ranum@me.com>
2017-09-26 13:48:20 +0200

    chore(server): set attribute to environment

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ if (isProduction) {
 const assetsUri = isProduction ? '/assets' : 'http://asgardr-client.app.dnt.local/assets';
 
 app.get('/', (req, res, next) => {
-  res.send(`<!doctype html><html><head><title>Den Norske Turistforening</title><meta charset="utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /></head><body class="editor"><div id="app" data-environment="development"></div><script type="text/javascript" src="${assetsUri}/app.js"></script></body></html>`);
+  res.send(`<!doctype html><html><head><title>Den Norske Turistforening</title><meta charset="utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /></head><body class="editor"><div id="app" data-environment="${process.env.NODE_ENV}"></div><script type="text/javascript" src="${assetsUri}/app.js"></script></body></html>`);
 });
 
 // Start the express app

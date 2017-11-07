27177b5a495af1ac2b61973b39ff4d37d7178f35
Håvard Ranum <havard.ranum@me.com>
2017-11-07 11:04:12 +0100

    chore(portal): show spinner while loading

--- client/src/apps/portal/scenes/Portal.jsx
+++ client/src/apps/portal/scenes/Portal.jsx
@@ -15,54 +15,62 @@ class Portal extends Component {
     return (
       <div>
         {
-          user.isAuthenticated === true
-            ? (
-              <Segment>
-                <Grid columns={2} relaxed>
-                  <Grid.Column>
+          (() => {
+            if (user.isFetching === true) {
+              return (
+                <Segment basic loading style={{minHeight: '100vh'}}></Segment>
+              );
+            } else if (user.isAuthenticated === true) {
+              return (
+                <Segment>
+                  <Grid columns={2} relaxed>
+                    <Grid.Column>
+                        <Segment basic textAlign="center">
+                          <a href="https://tur-dev.app.dnt.no/logout?next=/login/dnt/connect">
+                            <Icon name="map signs" size="huge" />
+                            <br /><br />
+                            <Button color="blue" content="Turforslag" icon='right arrow' labelPosition='right' />
+                          </a>
+                      </Segment>
+                    </Grid.Column>
+                    <Divider vertical>Eller</Divider>
+                    <Grid.Column>
                       <Segment basic textAlign="center">
-                        <a href="https://tur-dev.app.dnt.no/logout?next=/login/dnt/connect">
-                          <Icon name="map signs" size="huge" />
-                          <br /><br />
-                          <Button color="blue" content="Turforslag" icon='right arrow' labelPosition='right' />
-                        </a>
-                    </Segment>
-                  </Grid.Column>
-                  <Divider vertical>Eller</Divider>
-                  <Grid.Column>
-                    <Segment basic textAlign="center">
-                        <a href="https://hytte.app.dnt.no/auth/logout?next=/auth/login/dnt">
-                          <Icon name="bed" size="huge" color="brown" />
-                          <br /><br />
-                          <Button color="brown" content="Hytter" icon='right arrow' labelPosition='right' />
-                        </a>
-                    </Segment>
-                  </Grid.Column>
-                </Grid>
-              </Segment>
-            )
-            : (
-              <Segment basic textAlign="center">
-                <p style={{fontSize: '1.4em'}}>
-                  Dette er innlogging for DNT-foreninger, andre{' '}
-                  innholdspartnere og privatpersoner som skal publisere{' '}
-                  innhold på UT.no.
-                </p>
-                <p style={{fontSize: '1.4em'}}>
-                  Hvis du ikke har en DNT-bruker kan du{' '}
-                  <a href="https://www.dnt.no/minside/logg-inn/?next=/minside/#registrering">registrere deg</a>.
-                </p>
-                <Button
-                  as="a"
-                  href="/logg-inn"
-                  icon="user"
-                  labelPosition="left"
-                  content="Logg inn"
-                  color="grey"
-                  size="huge"
-                />
-              </Segment>
-            )
+                          <a href="https://hytte.app.dnt.no/auth/logout?next=/auth/login/dnt">
+                            <Icon name="bed" size="huge" color="brown" />
+                            <br /><br />
+                            <Button color="brown" content="Hytter" icon='right arrow' labelPosition='right' />
+                          </a>
+                      </Segment>
+                    </Grid.Column>
+                  </Grid>
+                </Segment>
+              );
+            } else {
+              return (
+                <Segment basic textAlign="center">
+                  <p style={{fontSize: '1.4em'}}>
+                    Dette er innlogging for DNT-foreninger, andre{' '}
+                    innholdspartnere og privatpersoner som skal publisere{' '}
+                    innhold på UT.no.
+                  </p>
+                  <p style={{fontSize: '1.4em'}}>
+                    Hvis du ikke har en DNT-bruker kan du{' '}
+                    <a href="https://www.dnt.no/minside/logg-inn/?next=/minside/#registrering">registrere deg</a>.
+                  </p>
+                  <Button
+                    as="a"
+                    href="/logg-inn"
+                    icon="user"
+                    labelPosition="left"
+                    content="Logg inn"
+                    color="grey"
+                    size="huge"
+                  />
+                </Segment>
+              );
+            }
+          })()
         }
       </div>
     );

617d7995f2f54a9faf9deef68439e0682d8dd3ab
Håvard Ranum <havard.ranum@me.com>
2017-11-09 15:49:16 +0100

    chore(rejecterror): simplify RejectError

--- client/src/lib/reject-error.js
+++ client/src/lib/reject-error.js
@@ -1,28 +1,8 @@
-// Custom error based on MDN docs
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#ES5_Custom_Error_Object
-
-function RejectError(message, fileName, lineNumber) {
-  const instance = new Error(message, fileName, lineNumber);
-
-  Object.setPrototypeOf(instance, Object.getPrototypeOf(this));
-  Error.captureStackTrace(instance, RejectError);
-
-  return instance;
+function RejectError(message) {
+  this.name = 'RejectError';
+  this.message = (message || '');
 }
 
-RejectError.prototype = Object.create(Error.prototype, {
-  constructor: {
-    value: Error,
-    enumerable: false,
-    writable: true,
-    configurable: true,
-  },
-});
-
-if (Object.setPrototypeOf) {
-  Object.setPrototypeOf(RejectError, Error);
-} else {
-  RejectError.__proto__ = Error; // eslint-disable-line no-proto
-}
+RejectError.prototype = Error.prototype;
 
 export default RejectError;

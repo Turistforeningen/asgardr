94c93d499335b9bf56869db0dcd43bffedbbefd6
Håvard Ranum <havard.ranum@me.com>
2017-11-16 13:57:00 +0100

    chore(header): add UT.no logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Menu} from 'semantic-ui-react';
 
-import logo from '../assets/images/dnt-logo-circle.svg';
+import dntLogo from '../assets/images/dnt-logo-circle.svg';
+import utnoLogo from '../assets/images/utno-logo-negative.svg';
 
 const Header = ({user}) => (
   <header>
@@ -9,14 +10,17 @@ const Header = ({user}) => (
       <div className="ui container">
         {
           user && user.isAuthenticated && user.data.is_admin ?
-            <a href="https://www.dnt.no/sherpa" className="header item logo">
-              <img src={logo} />
+            <a href="https://www.dnt.no/sherpa" className="header item logo dnt">
+              <img src={dntLogo} />
             </a>
           :
-            <a href="/" className="header item logo">
-              <img src={logo} />
+            <a href="/" className="header item logo dnt">
+              <img src={dntLogo} />
             </a>
         }
+        <a className="header item logo utno">
+          <img src={utnoLogo} />
+        </a>
         <Menu.Menu position="right">
           {
             user && user.isAuthenticated ?

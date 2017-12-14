2d018fdaef80c794730e38dff0002da7e500ceb2
Håvard Ranum <havard.ranum@me.com>
2017-12-14 14:25:59 +0100

    chore(conversion): update finish text

--- client/src/components/users/ConvertSuccess.jsx
+++ client/src/components/users/ConvertSuccess.jsx
@@ -5,11 +5,13 @@ const ConvertSuccess = ({turbasenUser, dntUser}) => (
   <Segment padded stacked textAlign="left">
     <h2>Gratulerer!</h2>
     <p>
-      Du er nå i mål. Neste gang du skal logge inn på UT velger du {' '}
-      «Logg inn med DNT-bruker».
+      Du er nå i mål. Neste gang du skal logge inn på UT går du til{' '}
+      <a href="https://admin.nasjonalturbase.no">https://admin.nasjonalturbase.no</a>{' '}
+      og velger «Logg inn med DNT-bruker». Husk å oppdatere bokmerkene dine!
     </p>
     <p>
-      Gå til f.eks. <a href="https://tur.app.dnt.no">turforslag</a> eller{' '}
+      Nå kan du gå videre til f.eks.{' '}
+      <a href="https://tur.app.dnt.no">turforslag</a> eller{' '}
       <a href="https://hytte.app.dnt.no">hytter</a>.
     </p>
   </Segment>
